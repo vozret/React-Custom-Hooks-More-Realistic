793d89b8db4c888fdb22eb64bf3d56296e76c3db
Veronika Ozretic <vozret00@fesb.hr>
2021-05-18 11:12:54 +0200

    Building a custom hook (190)

---
+++ src/hooks/use-http.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+// requestConfig -> an object that contains url and other
+// applyData -> a function of the Component which uses this hook
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      // request logic needs to be configurable
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading: isLoading, // or just isLoading
+    error: error, // or just error
+    sendRequest: sendRequest, // or just sendRequest
+  };
+};
+
+export default useHttp;
